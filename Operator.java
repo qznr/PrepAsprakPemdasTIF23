cb22cabc625b07d3f61e90b392f03e8c6c9a7795
Moch. Gustav Ali Rafsandjani <mgustav.penting@gmail.com>
2023-09-07 19:04:49 +0700

    asdf

--- Operator.java
+++ Operator.java
@@ -1,6 +1,7 @@
 public class Operator {
 
     public static void main(String[] args) {
+
         int angka = 100;
         int angkaDua = 200;
         System.out.println(angka + angkaDua);
@@ -10,43 +11,59 @@ public class Operator {
         System.out.println("100 - 200 = " + (angka - angkaDua));
         System.out.println("100 - 200 tanpa kurung = " + angka + -angkaDua);
         System.out.println(50*5);
-        System.out.println(50/2);
+        System.out.println(55f/2);
         System.out.println(200%5);
         System.out.println(201%5);
         System.out.println(202%5);
         System.out.println(203%5);
         System.out.println(204%5);
-        System.out.println(205%5);
+        System.out.println(205%5); // Operasi Modulo sama aplikasinya
 
-        angka++;
+        //Uninary operation
+        angka++; //increment
+        //variable++
+        //variable + 1
         System.out.println(angka);
         angka--;
         System.out.println(angka);
 
-        angka = angka + 1;
+        //Binary operation
+        angka = angka + 7;
         System.out.println(angka);
-        angka = angka - 1;
+        angka = angka - 17;
         System.out.println(angka);
 
-        angka += 1;
+        angka *= 2;
         System.out.println(angka);
-        angka -= 1;
+        angka -= 5;
+        System.out.println(angka);
 
         //Post increment
         System.out.println("\nPost Increment");
-        System.out.println(angka++);
-        System.out.println(angka);
-        System.out.println(angka--);
-        System.out.println(angka);
+        System.out.println(angka++); //angka++ -> 100+1 dilakukan setelah pencetakan. 100+1
+        System.out.println(angka); //kita cek
+        System.out.println(angka--);//angka-- -> 101-1 dilakukan setelah pencetakan selesai berjalan.
+        System.out.println(angka); //kita cek 100
+
         //Pre Increment
         System.out.println("Pre Increment");
-        System.out.println(++angka);
+        System.out.println(++angka); //melakukan operasi terlebih dahulu. ++angka -> 100 + 1 dilakukan dahulu sebelum pencetakan.
         System.out.println(--angka);
 
         //char juga bisa increment maupun decrement
         char huruf = 'a';
-        System.out.println(++huruf);
+        
+        System.out.println("asci a : " + (int) 'a');
+        System.out.println("asci b : " + (int) 'b');
+
         System.out.println(++huruf);
         System.out.println(--huruf);
+
+        System.out.println(huruf+=10);
     }
 }
+
+//bit
+//ascii
+//operasi modulo
+//post dan pre increment
